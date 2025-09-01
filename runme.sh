32ea10131348cde25219c340391d81c9ae3b361c
Jeff Loughridge <jeffbrl@gmail.com>
2025-09-01 17:00:10 +0000

    Improve runner script

--- runme.sh
+++ runme.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
+usage() { echo "Usage: $0 [-s <size_in_GB>] [-c <c_zip_file>] [-e <e_zip_file] -o <output_image>" 1>&2; exit 1; }
+
+while getopts ":s::o:c::e::" o; do
+    case "${o}" in
+        s)
+            size=${OPTARG}
+            ;;
+        o)
+            output=${OPTARG}
+            ;;
+        c)
+            czip=${OPTARG}
+            ;;
+        e)
+            ezip=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "$output" ]]; then
+    echo "Error: Specifying output image file with -o <file> is mandatory" >&2
+    usage
+    exit 1
+fi
+
+builder_args="/data/$output "
+
+if [[ -z "$size" ]]; then
+    echo "Output image size not specified...defaulting to 8G"
+    size=8
+    builder_args+="-s $size "
+fi
+
+if [[ -n "$czip" ]]; then
+    builder_args+="-c /data/$czip "
+fi
+
+if [[ -n "$ezip" ]]; then
+    builder_args+="-e /data/$ezip "
+fi
+
+echo $builder_args
 
 docker run --rm -v $(pwd)/main.py:/app/main.py \
--v $(pwd):/data jeffbrl/ogxbox-image-builder python3 /app/main.py /data/image.bin \
--c /data/c.zip -e /data/e.zip
+-v $(pwd):/data jeffbrl/ogxbox-image-builder python3 /app/main.py $builder_args
